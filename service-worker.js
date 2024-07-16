3e80b0717eb6cadfd91fc0512106c0aabf67c229
Abhijit <161806325+captain108@users.noreply.github.com>
2024-07-17 00:18:35 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,6 +7,61 @@ const urlsToCache = [
   '/storage/script.js'
 ];
 
+const dbName = 'my-site-db';
+const storeName = 'settings';
+const versionKey = 'version';
+
+function openDB() {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(dbName, 1);
+
+    request.onupgradeneeded = event => {
+      const db = event.target.result;
+      db.createObjectStore(storeName, { keyPath: 'name' });
+    };
+
+    request.onsuccess = event => {
+      resolve(event.target.result);
+    };
+
+    request.onerror = event => {
+      reject(event.target.error);
+    };
+  });
+}
+
+function getVersion(db) {
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction([storeName], 'readonly');
+    const objectStore = transaction.objectStore(storeName);
+    const request = objectStore.get(versionKey);
+
+    request.onsuccess = event => {
+      resolve(event.target.result ? event.target.result.value : null);
+    };
+
+    request.onerror = event => {
+      reject(event.target.error);
+    };
+  });
+}
+
+function setVersion(db, version) {
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction([storeName], 'readwrite');
+    const objectStore = transaction.objectStore(storeName);
+    const request = objectStore.put({ name: versionKey, value: version });
+
+    request.onsuccess = event => {
+      resolve();
+    };
+
+    request.onerror = event => {
+      reject(event.target.error);
+    };
+  });
+}
+
 self.addEventListener('install', event => {
   console.log('Service worker installing.');
   event.waitUntil(
@@ -57,11 +112,13 @@ setInterval(() => {
   console.log('Checking for updates...');
   fetch('/storage/version.txt')
     .then(response => response.text())
-    .then(version => {
+    .then(async version => {
       console.log('Current version:', version);
-      if (localStorage.getItem('version') !== version) {
+      const db = await openDB();
+      const storedVersion = await getVersion(db);
+      if (!storedVersion || storedVersion !== version) {
         console.log('New version detected. Updating...');
-        localStorage.setItem('version', version);
+        await setVersion(db, version);
         caches.keys().then(names => {
           names.forEach(name => caches.delete(name));
         });
@@ -75,4 +132,4 @@ setInterval(() => {
     .catch(error => {
       console.error('Error checking for updates:', error);
     });
-}, 15000); // Check every minute
+}, 60000); // Check every minute
